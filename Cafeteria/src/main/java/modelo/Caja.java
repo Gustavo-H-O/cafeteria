d965f0a17665d7e664e2f4c1505d3f5cbd1d188f
Gustavo-H-O <guhernandezor@est.utn.ac.cr>
2026-08-20 20:40:34 -0600

    creacion de la clase caja

---
+++ Cafeteria/src/main/java/modelo/Caja.java
@@ -0,0 +1,13 @@
+package com.utn.cafeteria.modelo;
+
+public class Caja {
+
+    private static final double TOLERANCIA = 0.01;
+
+    private double totalEfectivo;
+    private double totalTarjeta;
+    private int facturasEmitidas;
+    private boolean cerrada;
+
+
+}
